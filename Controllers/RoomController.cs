52e144df7aad7975a954484b4c2b39e98c4449de
apdlvmf8087 <apdlvmf8087@naver.com>
2025-07-21 18:22:06 +0900

    초기세팅

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,29 @@
+﻿using DefenseGameWebServer.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DefenseGameApiServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private static readonly RoomManager _roomManager = new(); // 싱글톤처럼 사용
+
+        [HttpPost("create")]
+        public IActionResult Create([FromForm] int userId)
+        {
+            var room = _roomManager.CreateRoom(userId);
+            return Ok(new { roomCode = room.RoomCode });
+        }
+
+        [HttpPost("join")]
+        public IActionResult Join([FromForm] int userId, [FromForm] string roomCode)
+        {
+            if (_roomManager.TryJoinRoom(roomCode, userId, out var room))
+            {
+                return Ok(new { roomCode = room.RoomCode });
+            }
+            return NotFound(new { message = "방이 존재하지 않습니다." });
+        }
+    }
+}
---
+++ Manager/RoomManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefenseGameWebServer.Manager
+{
+    public class RoomSession
+    {
+        public string RoomCode;
+        public int HostUserId;
+        public List<int> Participants = new();
+        public DateTime CreatedAt = DateTime.UtcNow;
+    }
+
+    public class RoomManager
+    {
+        private readonly Dictionary<string, RoomSession> _rooms = new();
+        private readonly Random _rand = new();
+
+        public RoomSession CreateRoom(int hostUserId)
+        {
+            string code;
+            do
+            {
+                code = GenerateRoomCode();
+            } while (_rooms.ContainsKey(code));
+
+            var room = new RoomSession
+            {
+                RoomCode = code,
+                HostUserId = hostUserId
+            };
+            room.Participants.Add(hostUserId);
+            _rooms[code] = room;
+
+            Console.WriteLine($"[Room Created] {code} by user {hostUserId}");
+            return room;
+        }
+
+        public bool TryJoinRoom(string code, int userId, out RoomSession room)
+        {
+            if (_rooms.TryGetValue(code, out room))
+            {
+                room.Participants.Add(userId);
+                Console.WriteLine($"[Room Join] user {userId} joined {code}");
+                return true;
+            }
+            return false;
+        }
+
+        private string GenerateRoomCode()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 6)
+                .Select(c => c[_rand.Next(c.Length)]).ToArray());
+        }
+    }
+}
